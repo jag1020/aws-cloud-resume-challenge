93553fe27d6fb6d25695405ca8c4773f27b563c6
Jordan Garcia <jgarcia@tradepmr.com>
2023-10-11 12:31:23 -0400

    first commit

---
+++ AWS website/js/scripts.js
@@ -0,0 +1,44 @@
+/*!
+* Start Bootstrap - Resume v7.0.6 (https://startbootstrap.com/theme/resume)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const sideNav = document.body.querySelector('#sideNav');
+    if (sideNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#sideNav',
+            rootMargin: '0px 0px -40%',
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
+
+// JavaScript Counter code
+const counter = document.querySelector(".counter-number");
+async function updateCounter() {
+    let response = await fetch("https://vhyoq534yfgh3l4jku2bjbgemm0prlcu.lambda-url.us-east-1.on.aws/");
+    let data = await response.json();
+    counter.innerHTML = ` Views: ${data}`;
+}
+
+updateCounter();
